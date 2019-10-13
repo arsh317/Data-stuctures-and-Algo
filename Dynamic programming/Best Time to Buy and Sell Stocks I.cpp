ff46d95a82aad808828c80ea5064069e3f5eed68
arsh317 <52664416+arsh317@users.noreply.github.com>
2019-10-13 10:00:11 +0530

    Update Best Time to Buy and Sell Stocks I.cpp

--- Dynamic programming/Best Time to Buy and Sell Stocks I.cpp
+++ Dynamic programming/Best Time to Buy and Sell Stocks I.cpp
@@ -1,6 +1,10 @@
 /* 
 Best Time to Buy and Sell Stocks I
-Say you have an array, A, for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit. Return the maximum possible profit.*/
+Say you have an array, A, for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
+design an algorithm to find the maximum profit. Return the maximum possible profit.
+
+*/
 
 
 int Solution::maxProfit(const vector<int> &A) {
